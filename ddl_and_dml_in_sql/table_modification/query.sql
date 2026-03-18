1020179fb8ca2dc24d6d6d235ea2556b22be142f
MatthewL09 <>
2026-03-18 18:54:08 +0000

    Table Modification

---
+++ ddl_and_dml_in_sql/table_modification/query.sql
@@ -0,0 +1,12 @@
+-- Write your solution here
+
+
+
+
+-- Please do not modify the code below!
+SELECT * FROM employees;
+
+TRUNCATE TABLE employees;
+
+ALTER TABLE employees DROP COLUMN country;
+-- Please do not modify the code above!
